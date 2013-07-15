06442b1437a67bfcb4362f8a2e9dc7305d8fcb00
James Austen <aus.james@gmail.com>
2013-07-15 22:08:25 +0100

    Initial setup
    Added style.scss, login page and header + footer templates [with no
    content]

---
+++ application/controllers/pages.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+  
+  public function __construct() {
+    parent::__construct();
+  }
+  
+  public function view($page = 'login') {
+    if (!file_exists('application/views/pages/'.$page.'.php')) {
+      show_404();
+    }
+    
+    $data['title'] = ucfirst($page);
+    
+    $this->load->view('templates/header.php', $data);
+    $this->load->view('pages/'.$page.'.php', $data);
+    $this->load->view('templates/footer.php', $data);
+  }
+  
+}
+?>
---
+++ application/views/pages/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mailing System</title>
+    <link rel="stylesheet" type="text/css" href="<?=base_url('style.css')?>" />
+    <script type="text/javascript" src="<?=base_url('script.js')?>"></script>
+  </head>
+
+  <body>
+    <h1>Mailing System Login</h1>
+  </body>
+</html>
